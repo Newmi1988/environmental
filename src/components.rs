81470035d70f90510584936805bda67dedc2bb71
Tobias Newmiwaka <newmi1988@gmail.com>
2023-12-23 17:03:18 +0100

    feat: specific components can be selected to output

---
+++ src/components.rs
@@ -0,0 +1,35 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct KeyValue {
+    name: String,
+    value: String
+}
+
+impl KeyValue {
+    fn to_env(&self) -> String {
+       format!("{0}={1}",self.name,self.value)
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Component {
+    pub(crate) name: String,
+    prefix: Option<String>,
+    values: Vec<KeyValue>
+}
+
+impl Component {
+    pub(crate) fn to_env(&self) -> Vec<String> {
+        let prefix_upper = match &self.prefix {
+            Some(x) => format!("{}_",x.to_uppercase()),
+            None => "".to_string()
+        };
+        let mut formatted_values: Vec<String> = Vec::new();
+        for value in &self.values {
+            let value_with_prefix= format!("{}{}",prefix_upper,value.to_env());
+            formatted_values.push(value_with_prefix);
+        }
+        formatted_values
+    }
+}
---
+++ src/config.rs
@@ -0,0 +1,34 @@
+use serde::{Deserialize, Serialize};
+use serde_yaml::from_str;
+use crate::components::Component;
+use crate::Mapping;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MentalConfig {
+    components: Vec<Component>,
+    mappings: Vec<Mapping>
+}
+
+impl MentalConfig {
+    pub(crate) fn to_env(&self, component_keys : Vec<String>) -> Vec<String> {
+        let filtered_components: Vec<&Component> = self.components
+            .iter()
+            .filter(|c| component_keys.contains(&c.name) )
+            .collect();
+
+        let mut combined_values: Vec<String> = Vec::new();
+        for c in filtered_components {
+            let component_output = c.to_env();
+            for x in component_output {
+                combined_values.push(x);
+            }
+        }
+        combined_values
+    }
+}
+
+pub fn load_config() -> MentalConfig {
+    let config_input = include_str!("../mental.yaml");
+    let config : MentalConfig = from_str(config_input).unwrap();
+    config
+}
